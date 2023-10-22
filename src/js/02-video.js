de2192705a0a2ffaaa99b5263d2bf0ea50b26404
NatHumeniuk <nat.gumenuk@gmail.com>
2023-10-22 18:24:35 +0300

    all tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(event.seconds)
+    );
+  }, 1000)
+);
